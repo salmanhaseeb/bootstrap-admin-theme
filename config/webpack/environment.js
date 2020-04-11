4373038076b2bd15a2d5261fa9899cd9da5a1e57
salmanhaseeb <salmanhaseeb@gmail.com>
2020-04-11 17:50:26 +0500

    added bootstrap admin-lte theme

---
+++ config/webpack/environment.js
@@ -0,0 +1,20 @@
+const { environment } = require('@rails/webpacker')
+const { VueLoaderPlugin } = require('vue-loader')
+const vue = require('./loaders/vue')
+
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+environment.loaders.prepend('vue', vue)
+
+
+const webpack = require('webpack')
+// Add an additional plugin of your choosing : ProvidePlugin
+environment.plugins.prepend('Provide', new webpack.ProvidePlugin({
+    $: 'jquery',
+    JQuery: 'jquery',
+    jquery: 'jquery',
+    'window.Tether': "tether",
+    Popper: ['popper.js', 'default'], // for Bootstrap 4
+  })
+)
+
+module.exports = environment
